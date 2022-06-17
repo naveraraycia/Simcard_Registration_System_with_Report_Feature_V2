bae0656842f0d7fb27ab9aad5cdcabf72455cc14
raycianavera <raycianavera@gmail.com>
2022-06-17 18:15:07 +0800

    DATA PRIVACY / SEPARATED VERIF / CHOOSE REGISTRATION

--- register-users-local.php
+++ register-users-local.php
@@ -34,15 +34,39 @@
   <!-- FONT AWESOME -->
   <script src="https://kit.fontawesome.com/207a28b81e.js" crossorigin="anonymous"></script>
 
+    <style>
+
+    /* BUTTONS */
+    .send-btn {
+      background-color: #2f5a62;
+      color: white;
+      font-weight: bold;
+      width: 100%;
+      height: 40px;
+      border-radius: 6px 6px 6px 6px;
+      position: relative;
+      margin-top: 1rem;
+      margin-bottom: 2rem;
+      border-width: 0;
+    }
+
+    .send-btn:hover {
+      background-color:#4b8f9c;
+      cursor: pointer;
+      color: white;
+    }
+
+
+    </style>
 </head>
-  <body>
+  <body style="background-color: white;">
     <!-- NAVBAR PART -->
     <header>
 
       <nav class="navbar navbar-expand-lg">
         <a class="div1 navbar-brand" href="register-users-local.php">
             <img src="images/logo.png" width="30" height="32" class="d-inline-block align-top" alt="">
-            <span class="brandname">SimCardRegistrationSystem</span>
+            <span class="brandname">SIM shop: Cavite SIM Shop</span>
           </a>
 
         <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -54,12 +78,8 @@
 
         <ul class='navbar-nav'>
               <li class='nav-item'>
-                <a class='nav-link selected' href='register-users-local.php'>Local User Registration</a>
+                <a class='nav-link selected' href='data-privacy-act.php'>Register User</a>
               </li>
-              <li class='nav-item'>
-                <a class='nav-link' href='register-users-foreign.php'>Foreign User Registration</a>
-              </li>
-
             </ul>
 
 
@@ -72,7 +92,7 @@
     </header>
 
     <!-- BODY PART -->
-    <div class="container">
+    <div class="container" style="background-color: #c8ece4;">
       <div class="row header">
             <h2>Local User Sim Card Registration Form</h2>
           </div>
@@ -123,209 +143,87 @@
 
 
 ?>
-<?php
-// BUTTON CLICKED : WITH RESULTS
-  if(isset($_GET['nsonum'])){
-    $nso = $_GET['nsonum'];
-    $query = "SELECT * FROM nso_dummy_db WHERE nsonum =  '$nso'; ";
-    $result = mysqli_query($conn,$query);
-
-      if (mysqli_num_rows($result) > 0) {
-        // if there is a result
-        foreach ($result as $row) {
-          ?>
-        <!-- FIRST ROW -->
-        <div class="row">
-
-          <div class="col-md-3 infodiv">
-            <label class="labelings">Last Name</label>
-            <input id="lastname" type="text" name="lastname" class="form-control" value="<?= $row['lastname'] ?>">
-          </div>
-
-          <div class="col-md-3 infodiv">
-            <label class="labelings">First Name</label>
-            <input id="firstname" type="text" name="firstname" class="form-control" value="<?= $row['firstname'] ?>">
-          </div>
-
-          <div class="col-md-3 infodiv">
-            <label class="labelings">Middle Name</label>
-            <input id="midname" type="text" name="midname" class="form-control" value="<?= $row['midname'] ?>">
-          </div>
-
-          <div class="col-md-3">
-            <label class="labelings">Suffix</label>
-            <input type="text" name="suffix" class="form-control" value="<?= $row['suffix'] ?>">
-          </div>
-
-        </div>
-
-        <!-- SECOND ROW -->
-        <div class="row srow">
-          <div class="col-md-3 infodiv">
-            <label class="labelings">Date of Birth</label>
-            <input id="dateofbirth" type="date" name="dateofbirth"  class="form-control" value="<?= $row['dateofbirth'] ?>" disabled>
-          </div>
-          <div class="col-md-3">
-            <label class="labelings">Gender</label>
-            <input  type="text" name="Gender"  class="Gender form-control" value="<?= $row['gender'] ?>" disabled>
-          </div>
-          <div class="col-md-6 infodiv">
-            <label class="labelings">NSO Barcode Number</label>
-            <input id="nsonum" type="text" name="nsonum" class="form-control"value="<?php
-              if (isset($_GET['nsonum'])) {
-                echo $_GET['nsonum'];
-                $_SESSION['nsonumber'] = $_GET['nsonum'];
-              }
-             ?>" required >
-          </div>
-
-        </div>
 
 
-        <!-- BUTTON ROW -->
-        <div class="row srow nsobutton">
-          <div class="col-12 infodiv">
-            <a href="register-users-local.php">
-            <button type="submit" name="button" class="send-btn db" onclick="register-users-local.php" >Search Database</button></a>
+   <form class="" action="#" method="post" enctype="multipart/form-data">
+     <!-- INITIAL = NOT YET PRESSING BUTTON SEARCH DATABASE : EMPTY FIELD -->
+     <?php
+     $nso = $_SESSION['nsonumber'];
+     $query = "SELECT * FROM nso_dummy_db WHERE nsonum =  '$nso'; ";
+     $result = mysqli_query($conn,$query);
 
-        </div>
+       if (mysqli_num_rows($result) > 0) {
+         // if there is a result
+         foreach ($result as $row) {
+           ?>
+         <!-- FIRST ROW -->
+         <div class="row">
 
-        </div>
-        </form>
-        <!-- END OF AUTOFILL -->
-        <form class="" action="includes/register_fingerprint.php" method="POST" enctype='multipart/form-data'>
-          <!-- FOURTH ROW -->
+           <div class="col-md-3 infodiv">
+             <label class="labelings">Last Name</label>
+             <input id="lastname" type="text" name="lastname" class="form-control" value="<?= $row['lastname'] ?>" disabled>
+           </div>
 
-          <div class="row">
-            <div class="col-12 infodiv">
-              <label class="Bday">Address</label>
-              <input id="address" type="text" name="address" class="form-control" required>
-            </div>
-          </div>
+           <div class="col-md-3 infodiv">
+             <label class="labelings">First Name</label>
+             <input id="firstname" type="text" name="firstname" class="form-control" value="<?= $row['firstname'] ?>" disabled>
+           </div>
 
-          <div class="row">
-            <div class="col-md-6">
-              <label class="labelings">Type of Sim Card user</label>
-              <select class="form-control" name="simcard">
-                <option value="new prepaid user">New prepaid user</option>
-                <option value="existing prepaid user">Existing prepaid user (Unregistered)</option>
-                <option value="postpaid user">Postpaid user</option>
-              </select>
-            </div>
-            <div class="col-md-6 infodiv">
-              <label class="labelings">Register SIM number</label>
-              <div class="input-group mb-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">+63</div>
-              </div>
-              <input type="tel" class="form-control" id="simnum" name="simnum" required>
-            </div>
-            </div>
-          </div>
+           <div class="col-md-3 infodiv">
+             <label class="labelings">Middle Name</label>
+             <input id="midname" type="text" name="midname" class="form-control" value="<?= $row['midname'] ?>" disabled>
+           </div>
 
-          <!-- FIFTH ROW -->
-          <div class="row srow">
-            <div class="col-md-6 infodiv">
-              <label class="labelings">Date of Registration</label>
-              <input id="dateregis"type="date" name="dateofregis" class="form-control" required>
-            </div>
+           <div class="col-md-3">
+             <label class="labelings">Suffix</label>
+             <input type="text" name="suffix" class="form-control" value="<?= $row['suffix'] ?>" disabled>
+           </div>
 
-            <div class="col-md-6 infodiv">
-              <label class="labelings">Registration Site</label>
-              <input id="regisite" type="text" name="regisite" class="form-control" placeholder="ex: Cavite" required>
-            </div>
-
-          </div>
-
-          <!-- PROCEED TO FINGERPRINT REGISTRATION BUTTON -->
-          <div class="row srow">
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="exampleFormControlFile1">Attach Fingerprint Image</label>
-                <input type="file" name="file" class="form-control-file" id="exampleFormControlFile1">
-              </div>
-            </div>
-
-            <div class="col-md-6">
-              <button type="submit" name="register" class="send-btn">Register User</button>
-        </div>
-        </div>
-
-        </form>
-        <?php
-        // DATA FROM AUTOFILL
-
-       ?>
-       <?php
-   }
-  } else {
-    // header("http://localhost/Sim-Registration-Final-UI-main/register-users-local.php?nsonum=.$nso.&button=no-result");
-    header("Location: ../Simcard_Registration_System_with_Report_Feature_V2/register-users-local.php?no-result=nsonum='.$nso.'&button");
-    // echo "NO RESULT";
-
-  }
-  }else {
-    // INITIAL = NOT YET PRESSING BUTTON SEARCH DATABASE : EMPTY FIELD
-  ?>
-  <form class="" action="register-users-local.php" method="GET">
-   <div class="row">
-    <div class="col-md-3">
-       <label class="labelings">Last Name</label>
-       <input type="text" name="lastname" class="form-control" disabled>
-
-       </div>
-
-       <div class="col-md-3">
-       <label class="labelings">First Name</label>
-       <input type="text" name="firstname" class="form-control" disabled>
-       </div>
-
-       <div class="col-md-3">
-       <label class="labelings">Middle Name</label>
-       <input type="text" name="midname" class="form-control" disabled>
-       </div>
-
-       <div class="col-md-3">
-       <label class="labelings">Suffix</label>
-       <input type="text" name="suffix" class="form-control" disabled>
-       </div>
-
-       </div>
-
-       <!-- SECOND ROW -->
-       <div class="row srow">
-         <div class="col-md-3 infodiv">
-           <label class="labelings">Date of Birth</label>
-           <input id="dateofbirth" type="date" name="dateofbirth"  class="form-control" disabled>
          </div>
-         <div class="col-md-3">
-           <label class="labelings">Gender</label>
-           <input  type="text" name="Gender"  class="Gender form-control" disabled>
+
+         <!-- SECOND ROW -->
+         <div class="row srow" style="margin-bottom: 2rem; margin-top: 1rem;">
+           <div class="col-md-3 infodiv">
+             <label class="labelings">Date of Birth</label>
+             <input id="dateofbirth" type="date" name="dateofbirth"  class="form-control" value="<?= $row['dateofbirth'] ?>" disabled>
+           </div>
+           <div class="col-md-3">
+             <label class="labelings">Gender</label>
+             <input  type="text" name="Gender"  class="Gender form-control" value="<?= $row['gender'] ?>" disabled>
+           </div>
+           <div class="col-md-6 infodiv">
+             <label class="labelings">NSO Barcode Number</label>
+             <input id="nsonum" type="text" name="nsonum" class="form-control"value="<?= $row['nsonum'] ?>" disabled >
+           </div>
+
          </div>
-         <div class="col-md-6 infodiv">
-           <label class="labelings">NSO Barcode Number</label>
-           <input id="nsonum" type="text" name="nsonum" class="form-control"value="<?php
-             if (isset($_GET['nsonum'])) {
-               echo $_GET['nsonum'];
-             }
-            ?>" required>
+
+
+         <?php
+
+       }
+     }
+
+
+     ?>
+
+
+       <div class="row">
+         <div class="col-md-6">
+           <div class="form-group">
+             <label for="nso-attach">Attach NSO</label>
+             <input type="file" name='file' class="form-control-file" id="nso-attach">
+           </div>
+         </div>
+
+         <div class="col-md-6">
+           <div class="form-group">
+             <label for="id-attach">Attach Valid ID</label>
+             <input type="file" name='file' class="form-control-file" id="id-attach">
+           </div>
          </div>
 
        </div>
-
-
-
-       <!-- BUTTON ROW -->
-       <div class="row srow nsobutton">
-         <div class="col-12 infodiv">
-         <button type="submit" name="button" class="send-btn db" onclick="register-users-local.php">Search Database</button>
-       </div>
-
-       </div>
-       </form>
-
-   <form class="" action="includes/register_fingerprint.php" method="post" enctype="multipart/form-data">
-       <!-- FOURTH ROW -->
 
        <div class="row">
          <div class="col-12 infodiv">
@@ -362,7 +260,7 @@
          </div>
 
          <div class="col-md-6 infodiv">
-           <label class="labelings">Registration Site</label>
+           <label class="labelings">Name of SIM retailer</label>
            <input id="regisite" type="text" name="regisite" class="form-control" placeholder="ex: Cavite" required>
          </div>
 
@@ -382,12 +280,13 @@
      </div>
      </div>
 
+
      </form>
 
 
       <?php
 
-  }
+
   ?>
 
 
