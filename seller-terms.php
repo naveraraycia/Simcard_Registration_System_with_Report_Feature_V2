800effc2e4eb50021fbb1755a7df211990f7a611
JasminDuenas <minduenas11@gmail.com>
2022-06-28 01:08:38 +0800

    edit

--- seller-terms.php
+++ seller-terms.php
@@ -1,32 +1,69 @@
 <?php
-  include_once 'dbh/EndUser.inc.php';
+  require 'includes/dbh.inc.php';
+
+?>
+<?php
   session_start();
-  ?>
-  <!DOCTYPE html>
-  <html lang="en" dir="ltr">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
+  if (empty($_SESSION['SellerFirstName'])){
+    header("Location: index.php");
+    exit();
+  }
+?>
+<!-- register-users-local.php?nsonum=3864&button= -->
+<!-- onclick="resetForm()" -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
 
-    <title>SimCardRegistrationSystem</title>
-    <!-- LOGO ON TAB -->
-    <link rel="icon" href="images/logo.png">
-    <!-- GOOGLE FONTS -->
-      <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;700&display=swap" rel="stylesheet">
-    <!-- CDN CSS FILE BOOTSTRAP VER 4.6 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+  <title>SimCardRegistrationSystem</title>
+  <!-- LOGO ON TAB -->
+  <link rel="icon" href="images/logo.png">
+  <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;700&display=swap" rel="stylesheet">
+  <!-- CDN CSS FILE BOOTSTRAP VER 4.6 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 
-    <!-- CUSTOM CSS FILE  -->
-    <link rel="stylesheet" href="styles/userprof.css">
-    <!-- FONT AWESOME -->
-    <script src="https://kit.fontawesome.com/207a28b81e.js" crossorigin="anonymous"></script>
+  <!-- CUSTOM CSS FILE  -->
+  <link rel="stylesheet" href="styles/register.css">
+  <!-- FONT AWESOME -->
+  <script src="https://kit.fontawesome.com/207a28b81e.js" crossorigin="anonymous"></script>
 
 
-  </head>
+<style>
+
+/* BUTTONS */
+.send-btn {
+  background-color: #2f5a62;
+  color: white;
+  font-weight: bold;
+  width: 100%;
+  height: 40px;
+  border-radius: 6px 6px 6px 6px;
+  position: relative;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+  border-width: 0;
+}
+
+.send-btn:hover {
+  background-color:#4b8f9c;
+  cursor: pointer;
+  color: white;
+}
+
+p{
+  line-height: 200%;
+}
+
+
+</style>
+</head>
   <body style="background-color: white;">
     <!-- NAVBAR PART -->
     <header>
@@ -46,7 +83,7 @@
 
           <ul class='navbar-nav'>
                 <li class='nav-item'>
-                  <a class='nav-link' href='data-privacy-act.php'>Register User</a>
+                  <a class='nav-link selected' href='data-privacy-act.php'>Register User</a>
                 </li>
 
                 <li class='nav-item'>
@@ -58,10 +95,13 @@
                 </li>
 
                 <li class='nav-item'>
-                  <a class='nav-link' href='request-sim-resupply.php'>Update info / Request SIM</a>
+                  <a class='nav-link' href='request-sim-resupply.php'>Request SIM</a>
                 </li>
 
               </ul>
+
+
+
 
           <form class="form-btnn" action="Logout/logoutprocess_SimRetailer.php" method="POST">
             <button type="submit" name="btn-primary" class="log-button">Logout</button>
